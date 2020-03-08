c07bbbce1ee7db5f47a424532458cb6945e29192
Mihai Rotaru <mihai.rotaru@gmx.com>
2020-03-08 22:26:37 +0000

    render to DOM (jsdom)

--- 00-vdom.jsx
+++ 00-vdom.jsx
@@ -1,3 +1,10 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const dom = new JSDOM(`<!DOCTYPE html><div id="root"></div>`);
+const window = dom.window;
+const document = window.document;
+
 const React = {
     createElement: (tag, props, ...children) => {
         if (typeof tag === "function") {
@@ -5,10 +12,32 @@ const React = {
         }
         const element = {
             tag,
-            props,
-            children,
+            props: {...props, children}
         };
         return element;
+    }
+}
+
+const render = (reactElement, domContainer) => {
+    if (Array.isArray(reactElement)) {
+        reactElement.forEach(element => {
+            render(element, domContainer);
+        })
+    } else if (typeof reactElement === "string") {
+        domContainer.textContent = reactElement;
+    } else {
+        const { tag, props } = reactElement;
+        const { children } = props;
+        let domElement = document.createElement(tag);
+        Object.keys(props).filter(key => key !== "children").forEach(key => {
+            domElement[key] = props[key];
+        })
+        if (children) {
+            children.forEach(child => {
+                render(child, domElement);
+            })
+        }
+        domContainer.appendChild(domElement);
     }
 }
 
@@ -26,10 +55,14 @@ const WithChildren = <App>
     </div>
 </App>
 
-log(WithChildren);
+logVdom(WithChildren);
+
+render(WithChildren, document.querySelector("#root"));
+
+console.log(document.documentElement.outerHTML);
 
 // utilities
 
-function log(component) {
+function logVdom(component) {
     console.log(JSON.stringify(component, null, 2));
 }
